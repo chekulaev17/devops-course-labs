fb6ffbc33827eac776a2787f919833206c6be927
chekulaev17 <chekulaev17@gmail.com>
2026-05-09 07:01:38 -0400

    add docker deployment

---
+++ deploy.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+set -e
+
+IMAGE_NAME=$1
+DEPLOY_REF=$2
+
+HOST_PORT=8181
+CONTAINER_PORT=8181
+CONTAINER_NAME="catty-reminders-app"
+
+IMAGE="$IMAGE_NAME:$DEPLOY_REF"
+
+docker pull $IMAGE
+
+docker stop $CONTAINER_NAME || true
+docker rm $CONTAINER_NAME || true
+
+docker run -d 
+-p $HOST_PORT:$CONTAINER_PORT 
+--name $CONTAINER_NAME 
+--restart unless-stopped 
+-e DEPLOY_REF=$DEPLOY_REF 
+$IMAGE
