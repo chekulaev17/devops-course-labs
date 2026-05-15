e43ffd2e71056d47ae3b0e293c5351ea686aa29e
chekulaev17 <chekulaev17@gmail.com>
2026-05-15 08:34:07 -0400

    fix ci cd deploy

--- deploy.sh
+++ deploy.sh
@@ -5,17 +5,31 @@ set -e
 IMAGE_NAME=$1
 DEPLOY_REF=$2
 
+HOST_PORT=8181
+CONTAINER_PORT=8181
+
+CONTAINER_NAME="catty-reminders-app"
+
+IMAGE="$IMAGE_NAME:$DEPLOY_REF"
+
 echo "=== DEPLOY RELEASE ==="
 echo "IMAGE_NAME=$IMAGE_NAME"
 echo "DEPLOY_REF=$DEPLOY_REF"
 
-sudo docker pull $IMAGE_NAME:$DEPLOY_REF
+echo "=== Pull image ==="
+sudo docker pull $IMAGE
 
-sudo docker stop catty-reminders-app || true
-sudo docker rm catty-reminders-app || true
+echo "=== Stop old container ==="
+sudo docker stop $CONTAINER_NAME || true
+
+echo "=== Remove old container ==="
+sudo docker rm $CONTAINER_NAME || true
+
+echo "=== Start new container ==="
 
 sudo docker run -d \
-  -p 8181:8181 \
-  --name catty-reminders-app \
+  -p $HOST_PORT:$CONTAINER_PORT \
+  --name $CONTAINER_NAME \
   --restart unless-stopped \
-  $IMAGE_NAME:$DEPLOY_REF
+  $IMAGE
+
